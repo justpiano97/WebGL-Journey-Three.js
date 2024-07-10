bcde06ab8e64dda37dd9edba4c691bec7dc7fe5c
justpiano <topdeveloper97@gmail.com>
2024-07-10 05:21:48 +0300

    add: eslint and prettier configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,13 +6,20 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: [
+    'react-refresh', 
+    'prettier'],
   rules: {
+    'prettier/prettier': ['error', {
+      'semi': true
+    }],
     'react/jsx-no-target-blank': 'off',
+    'no-unused-vars': 'warn',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
